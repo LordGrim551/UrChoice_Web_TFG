340cab91a23b3469ee03bf235ebf0a774a9e5210
LucaCampusfp <lucacampusfp@gmail.com>
2025-04-20 11:19:46 +0200

    correción

--- UrChoice/src/Pages/HomePage/FriendBar/FriendBar.jsx
+++ UrChoice/src/Pages/HomePage/FriendBar/FriendBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './FriendBar.css';
+// import './FriendBar.css';
 
 const FriendBar = () => {
     const [friends, setFriends] = useState([]);
