f345ee0e30b7f5510ae47223201c788824dd3b63
LucaCampusfp <lucacampusfp@gmail.com>
2025-04-22 02:09:52 +0200

    ajuste

--- UrChoice/src/Pages/HomePage/HomePage.jsx
+++ UrChoice/src/Pages/HomePage/HomePage.jsx
@@ -37,7 +37,7 @@ function HomePage() {
       </aside>
       <main className="flex flex-grow gap-4">
         {/* Contenido principal */}
-        <div className="category-card w-full h-[calc(87vh-48px)] flex-grow p-4  border-red-600 border-5 rounded-lg overflow-y-auto scrollbar-custom">
+        <div className="category-card w-full h-[calc(86vh-48px)] flex-grow p-4  border-red-600 border-5 rounded-lg overflow-y-auto scrollbar-custom">
           <p className="text-center text-lg font-bold text-gray-700">
             Bienvenido, {nick ? `Nick: ${nick}` : 'Cargando usuario...'}
           </p>
@@ -51,7 +51,7 @@ function HomePage() {
         </div>
 
         {/* Barra de amigos en pantallas grandes (desplazamiento vertical) */}
-        <div className="hidden lg:flex w-1/5 max-h-[calc(87vh-48px)] overflow-y-auto">
+        <div className="hidden lg:flex w-1/5 max-h-[calc(86vh-48px)] overflow-y-auto">
           <FriendBar />
         </div>
       </main>
