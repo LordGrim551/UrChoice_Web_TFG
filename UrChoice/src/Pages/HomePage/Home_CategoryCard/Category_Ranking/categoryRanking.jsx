d6adc76dfc46c754bbe5a4414bd9f784c8d660f8
LucaCampusfp <lucacampusfp@gmail.com>
2025-05-04 20:57:36 +0200

    muchos avances

--- UrChoice/src/Pages/HomePage/Home_CategoryCard/Category_Ranking/categoryRanking.jsx
+++ UrChoice/src/Pages/HomePage/Home_CategoryCard/Category_Ranking/categoryRanking.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import {  ScatterChart,  Scatter,  XAxis,  YAxis,  CartesianGrid,  Tooltip,  ResponsiveContainer,  LabelList} from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList, Line } from 'recharts';
 import Logo from './logoHeader.png';
 
 const Ranking = ({ categoryId, handleBack }) => {
   const COLORS = ['#8884d8', '#82ca9d', '#ffc658', '#FF8042', '#00C49F'];
-  const fetchRanking = async() =>{
-    
-  }
+  const [rankings, setRankings] = useState([]);
+  const [categoryImage, setCategoryImage] = useState('');
+  const fetchRanking = async (categoryId) => {
+    const response = await fetch(`https://railwayserver-production-7692.up.railway.app/elements/ranking/${categoryId}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    });
 
-  const rankings = [
-    { id: 1, name: 'card 1', score: 95, image: Logo },
-    { id: 2, name: 'card 2', score: 90, image: Logo },
-    { id: 3, name: 'card 3', score: 50, image: Logo },
-    { id: 4, name: 'card 4', score: 80, image: Logo },
-    { id: 5, name: 'card 5', score: 75, image: Logo },
-  ];
+    const data = await response.json();
+    if (response.ok && Array.isArray(data)) {
+      const formattedCategories = data.map(categoryElement => ({
+        id: categoryElement.id_elem,
+        name: categoryElement.name_elem,
+        score: categoryElement.victories ?? 0,
+        image: `data:image/png;base64,${categoryElement.img_elem}`
+      }));
+      setRankings(formattedCategories);
+    }
+  };
+  const fetchCategoryImage = async (categoryId) => {
+    const response = await fetch(
+      `https://railwayserver-production-7692.up.railway.app/category/${categoryId}`
+      );
+    const data = await response.json();
+    if (response.ok) {
+      const imageBase64 = `data:image/png;base64,${data.img_cat}`;
+      setCategoryImage(imageBase64);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchRanking(categoryId);
+    fetchCategoryImage(categoryId);
+  }, [categoryId]);
+
 
   const maxScore = Math.max(...rankings.map(item => item.score));
 
@@ -27,43 +52,55 @@ const Ranking = ({ categoryId, handleBack }) => {
     lineColor: COLORS[item.id % COLORS.length]
   }));
 
-  const CustomImageComponent = ({ x, y, payload, color }) => {
+  const CustomImageComponent = ({ cx, cy, payload, color }) => {
     const startX = 100;
-    const startY = y;
+    const startY = cy;
 
     return (
       <g>
-        {/* Línea horizontal desde el eje X hasta el punto */}
-        <line x1={startX} y1={startY} x2={x} y2={y} stroke={color} strokeWidth={2} />
-        <circle cx={startX} cy={startY} r={5} fill={color} />
-        <image
-          x={x - 15}
-          y={y - 15}
-          width={30}
-          height={30}
-          xlinkHref={payload.image}
+        <line x1={startX} y1={startY} x2={cx} y2={cy} stroke={color} strokeWidth={2} />
+        <Line
+          x1={0}
+          y1={cy}
+          x2={cx}
+          y2={cy}
+          stroke={payload.lineColor}
+          strokeWidth={4}
+          style={{ zIndex: -1 }}
         />
+
+        <foreignObject x={cx - 16} y={cy - 16} width={64} height={64}>
+          <div className="w-16 h-8  rounded-full border-2 border-cyan-400 overflow-hidden ">
+            <img
+              src={payload.image}
+              alt="card"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </foreignObject>
       </g>
     );
   };
 
   return (
     <div className="border-2 border-cyan-400 p-4 rounded-lg w-full mx-auto overflow-y-auto scrollbar-custom max-h-[78vh] ">
-      <img src="asddas" alt="category image" className='border-2 border-cyan-400 h-32 w-full rounded-t-2xl' />
-      <div className="w-full bg-cyan-400 h-24 mb-5 flex justify-center items-center">Ranking category ID: {categoryId}</div>
+
+      <img src={categoryImage} alt="category image" className='border-2 border-cyan-400 h-32 w-full rounded-t-2xl object-cover ' />
+
+      <div className="w-full bg-cyan-400 h-24 mb-5 flex justify-center text items-center font-bold text-4xl">Ranking category ID: {categoryId}</div>
       <div className="flex flex-col md:flex-row gap-4">
         <div className="w-full h-auto border-2 border-cyan-400 rounded-2xl md:w-1/2">
           {rankings.map((item, index) => (
             <div key={item.id} className="flex items-center border border-cyan-400 py-2 mb-4 m-5 rounded-2xl">
               <div className="w-10 text-center">{index + 1}</div>
-              <img src={item.image} alt={item.name} className="w-16 h-16 rounded-full" />
+              <img src={item.image} alt={item.name} className="w-16 h-16 rounded-full object-cover" />
               <div className="ml-4 font-semibold">{item.name}</div>
               <div className="ml-auto mr-2">Score: {item.score}</div>
             </div>
           ))}
         </div>
         <div className="w-full md:w-1/2 h-auto border-2 border-cyan-400 rounded-2xl">
-          <ResponsiveContainer width="100%" height={400}>
+          <ResponsiveContainer width="100%" height="100%">
             <ScatterChart
               margin={{
                 top: 20,
@@ -77,7 +114,7 @@ const Ranking = ({ categoryId, handleBack }) => {
                 type="number"
                 dataKey="x"
                 name="Score"
-                domain={[0, 100]}
+                domain={[0, 10]}
                 label={{ value: 'Puntuación', position: 'bottom' }}
               />
               <YAxis
