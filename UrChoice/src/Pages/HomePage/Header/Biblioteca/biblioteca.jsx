9d80dcfdd5931f9eaeaad132df8a637f35898780
LucaCampusfp <lucacampusfp@gmail.com>
2025-05-05 02:52:02 +0200

    cambios detalles

--- UrChoice/src/Pages/HomePage/Header/Biblioteca/biblioteca.jsx
+++ UrChoice/src/Pages/HomePage/Header/Biblioteca/biblioteca.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Favoritos from './favoritos';
 import Guardados from './guardados'
 import { motion, AnimatePresence } from 'framer-motion';
