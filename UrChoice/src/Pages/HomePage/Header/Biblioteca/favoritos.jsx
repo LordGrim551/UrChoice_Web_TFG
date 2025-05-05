6649fe16c707093e47682a4e362659d05123cc20
LucaCampusfp <lucacampusfp@gmail.com>
2025-05-05 03:06:40 +0200

    subida

--- UrChoice/src/Pages/HomePage/Header/Biblioteca/favoritos.jsx
+++ UrChoice/src/Pages/HomePage/Header/Biblioteca/favoritos.jsx
@@ -13,7 +13,7 @@ const Favoritos = () => {
   }, []);
 
   const fetchFavourites = async (id_user) => {
-      try {
+    try {
       const response = await fetch(`https://railwayserver-production-7692.up.railway.app/favoritos/${id_user}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
@@ -50,18 +50,34 @@ const Favoritos = () => {
 
 
   return (
-    <div className='m-2'>
-      {favoritos.length === 0 ? (
-        <div className="text-center">No tienes categorias favoritas prueba la gran variedad de categorias o personaliza creando las tuyas propias</div>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {favoritos.map((item) => (
-            <div key={item.id_fav} className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
-              <img src={item.img_cat} alt={item.name_cat} className="w-full h-40 object-cover rounded-md mb-2" />
-              <h3 className="text-lg font-medium">{item.name_cat}</h3>
-          <Heart className="text-blue-500 mt-2" />
-            </div>))}
-        </div>)}
+   
+
+    <div className="w-full category-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  p-4 overflow-y-auto scrollbar-custom max-h-[70vh]">
+      {favoritos.map((item) => (
+        <div
+          key={item.id_fav}
+          className="category-card border  border-gray-300 rounded-lg shadow-md cursor-pointer"
+
+        >
+
+          <div className="card-header bg-red-500 text-white rounded-t-lg p-2 text-center">
+            {item.name_cat}
+          </div>
+          <div className="relative w-full aspect-[4/3] overflow-hidden">
+            <img
+              src={item.img_cat}
+              alt={item.name_cat}
+              className="w-full h-full object-cover"
+              onClick={() => onCategoryClick(item.id_cat)}
+            />
+          </div>
+          <div className="flex items-center justify-evenly card-footer bg-cyan-500 text-white rounded-b-lg p-2 text-center">
+            ID: {item.id_cat}
+            <Heart size={24} />
+  
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
