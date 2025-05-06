1f5afb3695c05554aa29583796427e5033d886f1
LucaCampusfp <lucacampusfp@gmail.com>
2025-05-06 13:00:39 +0200

    eliminar vulnerabilidades

--- UrChoice/vite.config.js
+++ UrChoice/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ]
+    removeConsole(),
+  
+  ],
+  esbuild: {
+    drop: ['console', 'debugger']
+  }
 })
