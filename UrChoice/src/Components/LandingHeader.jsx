4ef598b263fe20eeee34bbd423710532eaaff59e
LucaCampusfp <lucacampusfp@gmail.com>
2025-07-28 19:59:11 +0200

    Tu mensaje descriptivo aquí

--- UrChoice/src/Components/LandingHeader.jsx
+++ UrChoice/src/Components/LandingHeader.jsx
@@ -1,22 +1,43 @@
 import { useState } from 'react'
-import Logo from '../assets/LogoTodoSVG.svg'
+import Logo from '../assets/logo.svg'
+import { Home, Gamepad, Wrench } from 'lucide-react';
 
 function LandingHeader() {
 
     return (
-        <header className="bg-gradient-to-b from-red-500  to-black
-        flex items-center
+        <header className="bg-gradient-to-b
+        from-red-500 
+        to-black
+        flex items-center 
         ">
-            <div className="image-logo w-24">
-                <img src={Logo} alt="Logo"/>
+            <div className="image-logo w-24 m-2">
+                <picture>
+                    <img srcSet={Logo} alt="Logo" />
+                </picture>
             </div>
-            <nav className='navbar'>
-                <ul className='flex justify-between'>
-                    <li>Home</li>
-                    <li>Categories</li>
-                    <li>Tools and Extras</li>
+            <nav className='navbar w-full'>
+                <ul className='flex justify-center space-x-12'>
+                    <li>
+                        <button className="flex items-center space-x-2">
+                            <Home />
+                            <span>Inicio</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="flex items-center space-x-2">
+                            <Gamepad />
+                            <span>Categories</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="flex items-center space-x-2">
+                            <Wrench />
+                            <span>Tools</span>
+                        </button>
+                    </li>
                 </ul>
             </nav>
+
         </header>
     )
 }
