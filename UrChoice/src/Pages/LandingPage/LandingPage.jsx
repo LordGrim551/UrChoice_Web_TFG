c38530865b5fd2797cb9811d877864fb51a87652
LucaCampusfp <lucacampusfp@gmail.com>
2025-07-29 23:35:24 +0200

    arreglar tema height

--- UrChoice/src/Pages/LandingPage/LandingPage.jsx
+++ UrChoice/src/Pages/LandingPage/LandingPage.jsx
@@ -2,17 +2,16 @@ import LandingHeader from "../../Components/LandingHeader";
 import HeroSection from "../../Components/HeroSection";
 import Description from "../../Components/Description";
 import CategoryCards from "../../Components/CategoryCards";
-import '../LandingPage/LandingPage.css';
+
 function LandingPage() {
     return (
-        <>
-            <LandingHeader></LandingHeader>
-            <HeroSection></HeroSection>
-            <Description></Description>
-            <CategoryCards></CategoryCards>
-        </>
-
-    )
+        <div className="min-h-[200vh] bg-black m-0 p-0 sm:min-h-[400vh]">
+            <LandingHeader />
+            <HeroSection />
+            <Description />
+            <CategoryCards />
+        </div>
+    );
 }
 
 export default LandingPage;
