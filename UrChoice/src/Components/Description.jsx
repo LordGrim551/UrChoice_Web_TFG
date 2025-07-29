b85e4d48e8412df8b169172ca0b541fb4b63c726
LucaCampusfp <lucacampusfp@gmail.com>
2025-07-29 19:23:52 +0200

    testimonial succed

---
+++ UrChoice/src/Components/Description.jsx
@@ -0,0 +1,21 @@
+import '../Components/css/Descriptions.css'
+
+function Description() {
+
+  return (
+    <>
+      <div id="testimonial" className="flex flex-col items-center justify-center absolute inset-x-0 top-114 ">
+
+        <span className="text-white font-bold text-2xl w-114 ">
+          “In UrChoice, create your own deck of cards with custom categories, play with friends, and discover the community’s favorite cards.”</span>
+        <span className="w-full text-white font-extralight font-stretch-ultra-expanded">"Development team"</span>
+      </div>
+
+
+
+
+    </>
+  );
+
+}
+export default Description;
--- UrChoice/src/Pages/LandingPage/LandingPage.jsx
+++ UrChoice/src/Pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import LandingHeader from "../../Components/LandingHeader";
 import HeroSection from "../../Components/HeroSection";
+import Description from "../../Components/Description";
 function LandingPage() {
     return (
         <>
             <LandingHeader></LandingHeader>
             <HeroSection></HeroSection>
+            <Description></Description>
         </>
 
     )
