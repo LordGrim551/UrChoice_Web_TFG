3761460dd592fe636b93a8fa410c50730c494f3a
LucaCampusfp <lucacampusfp@gmail.com>
2025-07-31 01:15:59 +0200

    span arreglar

--- UrChoice/src/Components/Description.jsx
+++ UrChoice/src/Components/Description.jsx
@@ -4,16 +4,12 @@ function Description() {
 
   return (
     <>
-      <div id="testimonial" className="flex flex-col items-center justify-center m-32">
-
-        <span className="text-white font-bold text-l w-auto ml-16 mr-16 sm:text-2xl ">
-          “In UrChoice, create your own deck of cards with custom categories, play with friends, and discover the community’s favorite cards.”</span>
-        <span className="w-full text-white font-extralight">"Development team"</span>
+      <div id="testimonial" className="flex flex-col items-center justify-center m-8 sm:m-16 md:m-32">
+        <span className="text-white font-bold text-lg sm:text-xl md:text-2xl w-full max-w-2xl text-center px-4">
+          “In UrChoice, create your own deck of cards with custom categories, play with friends, and discover the community’s favorite cards.”
+        </span>
+        <span className="w-full text-white font-extralight text-center mt-2">"Development team"</span>
       </div>
-
-
-
-
     </>
   );
 
