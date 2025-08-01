a28660f90f26f52e542e0f005387d6a137008cf9
LucaCampusfp <lucacampusfp@gmail.com>
2025-08-01 21:59:31 +0200

    lazy loading

--- UrChoice/src/App.jsx
+++ UrChoice/src/App.jsx
@@ -1,43 +1,46 @@
-import Preloader from '../src/Pages/Preloader/Preloader';
-import TapToStart from '../src/Pages/TapToStart/TapToStart';
-import InitialPage from '../src/Pages/InitialPage/InitialPage';
-import LogInPage from '../src/Pages/LoginPage/LoginPage';
-import RegisterPage from '../src/Pages/RegisterPage/RegisterPage';
-import HomePage from '../src/Pages/HomePage/HomePage';
-import Error from '../src/Pages/Error/Error';
-import Game from '../src/Pages/GamePage/Game';
-import Individual from '../src/Pages/GamePage/IndividualGame';
-import Biblioteca from './Pages/HomePage/Header/Biblioteca/biblioteca';
-import User from './Pages/HomePage/Header/User/User';
-import TabView from './Pages/HomePage/Tabview/tabview'; // Añade esta importación
-import LandingPage from './Pages/LandingPage/LandingPage';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Lazy loading de las páginas
+const Preloader = lazy(() => import('../src/Pages/Preloader/Preloader'));
+const TapToStart = lazy(() => import('../src/Pages/TapToStart/TapToStart'));
+const InitialPage = lazy(() => import('../src/Pages/InitialPage/InitialPage'));
+const LogInPage = lazy(() => import('../src/Pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('../src/Pages/RegisterPage/RegisterPage'));
+const HomePage = lazy(() => import('../src/Pages/HomePage/HomePage'));
+const ErrorPage = lazy(() => import('../src/Pages/Error/Error'));
+const Game = lazy(() => import('../src/Pages/GamePage/Game'));
+const Individual = lazy(() => import('../src/Pages/GamePage/IndividualGame'));
+const Biblioteca = lazy(() => import('./Pages/HomePage/Header/Biblioteca/biblioteca'));
+const User = lazy(() => import('./Pages/HomePage/Header/User/User'));
+const TabView = lazy(() => import('./Pages/HomePage/Tabview/tabview'));
+const LandingPage = lazy(() => import('./Pages/LandingPage/LandingPage'));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/Preloader" element={<Preloader />} />
-        <Route path="/Tap_to_start" element={<TapToStart />} />
-        <Route path="/InitialPage" element={<InitialPage />} />
-        <Route path="/LogInPage" element={<LogInPage />} />
-        <Route path="/RegisterPage" element={<RegisterPage />} />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Preloader" element={<Preloader />} />
+          <Route path="/Tap_to_start" element={<TapToStart />} />
+          <Route path="/InitialPage" element={<InitialPage />} />
+          <Route path="/LogInPage" element={<LogInPage />} />
+          <Route path="/RegisterPage" element={<RegisterPage />} />
 
-        {/* Rutas anidadas para el layout de HomePage */}
-        <Route path="/HomePage" element={<HomePage />}>
-          <Route index element={<TabView />} /> {/* Usa el componente importado */}
-          <Route path="Biblioteca" element={<Biblioteca />} />
-          <Route path="User" element={<User />} />
-        </Route>
+          <Route path="/HomePage" element={<HomePage />}>
+            <Route index element={<TabView />} />
+            <Route path="Biblioteca" element={<Biblioteca />} />
+            <Route path="User" element={<User />} />
+          </Route>
 
-        <Route path="/GamePage" element={<Game />} />
-        <Route path="/IndividualGame" element={<Individual />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
+          <Route path="/GamePage" element={<Game />} />
+          <Route path="/IndividualGame" element={<Individual />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </Router>
-  )
-  
+  );
 }
 
 export default App;
